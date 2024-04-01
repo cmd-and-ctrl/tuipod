c8ea0d981f99be278b62490ea50246f50733ccc6
keeperofkey <doddtastic@gmail.com>
2024-04-01 10:52:07 -0600

    feat(apmusic): fixed import

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
@@ -14,18 +15,19 @@ import (
 
 type model struct {
 	list      list.Model
-	spinner   spinner.Model 
+	spinner   spinner.Model
 	textInput textinput.Model
+	currentTrack *applemusic.Track
 
 	ipodUI *IPodUI
 }
 
 func initialModel() model {
 	return model{
-		list: list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0),
-		spinner: spinner.New(),
-		textInput: textinput.New(), 
-                currentTrack: go-apple-music.Track,
+		list:      list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0),
+		spinner:   spinner.New(),
+		textInput: textinput.New(),
+		currentTrack: &applemusic.Track{},
 
 		ipodUI: NewIPodUI(),
 	}
@@ -39,23 +41,22 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 
 	switch msg := msg.(type) {
-	// Handle messages
-
 	case tea.KeyMsg:
 		switch msg.String() {
-		// Handle key presses	
+		case "ctrl+c", "q":
+			return m, tea.Quit
 		}
 	}
 
 	m.ipodUI.Render() // Render UI
 
-	return m, cmd  
+	return m, cmd
 }
 
 func main() {
 	p := tea.NewProgram(initialModel())
 	if err := p.Start(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
-		os.Exit(1) 
+		os.Exit(1)
 	}
 }
